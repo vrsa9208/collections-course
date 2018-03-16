65c76767c183ebcfdcb84687deb325bf534c7f69
vvo0002 <vvo002@mx.wal-mart.com>
2018-03-16 16:34:13 -0600

    Basic operations for shipments

--- src/main/java/com/walmart/development/collections/course/console/Console.java
+++ src/main/java/com/walmart/development/collections/course/console/Console.java
@@ -1,5 +1,11 @@
 package com.walmart.development.collections.course.console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.walmart.development.collections.course.units.three.Product;
+
 /**
  * @author vvo0002
  *
@@ -13,6 +19,28 @@ public class Console
   public static void main(String[] args)
   {
     System.out.println("Java Collections");
+    Product door = new Product("Wooden door", 35);
+    Product floorPanel = new Product("Floor panel", 25);
+    Product window = new Product("Glass window", 10);
+    
+  }
+
+
+
+  private static void sublist()
+  {
+    List<String> listaSimple = new ArrayList();
+    listaSimple.add("A");
+    listaSimple.add("B");
+    listaSimple.add("C");
+    listaSimple.add("D");
+    listaSimple.add("E");
+    listaSimple.add("F");
+    listaSimple.add("G");
+    List<String> sublistaSimple = listaSimple.subList(1, 3);
+    sublistaSimple.add("C1");
+    System.out.println(listaSimple);
+    System.out.println(sublistaSimple);
   }
 
 }
