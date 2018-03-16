6ce8eb5e2731b831d1ce677f925b07f0c49f83fe
vvo0002 <vvo002@mx.wal-mart.com>
2018-03-16 13:44:03 -0600

    chore: Start of the project

---
+++ src/main/java/com/walmart/development/collections/course/console/Console.java
@@ -0,0 +1,18 @@
+package com.walmart.development.collections.course.console;
+
+/**
+ * @author vvo0002
+ *
+ */
+public class Console
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Java Collections");
+  }
+
+}
